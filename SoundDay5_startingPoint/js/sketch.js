0c036f98222f9cda5b23090ab1ae0558192707c2
Nick-LeBlanc <nickleblanc811@gmail.com>
2023-03-07 20:26:58 -0600

    noise

--- SoundDay5_startingPoint/js/sketch.js
+++ SoundDay5_startingPoint/js/sketch.js
@@ -10,7 +10,24 @@
 // }).connect(pan);
 // osc.connect(ampEnv);
 
-const plucky = new Tone.PluckSynth()
+const plucky = new Tone.PluckSynth();
+const pitch  = new Tone.PitchShift(-1);
+
+const notes = [
+    { pitch: "G4", timing: 0 },
+    { pitch: "F4", timing: 0.1 },
+    { pitch: "E4", timing: 0.1 },
+    { pitch: "D4", timing: 0.1 },
+    { pitch: "C4", timing: 0.1 }
+];
+
+function play() {
+    let delay = Tone.now();
+    for(let i = 0; i < notes.length; i++) {
+        delay += notes[i].timing;
+        plucky.triggerAttackRelease(notes[i].pitch, '8n', delay);  
+    }
+}
 
 let img1;
 let img2;
@@ -24,23 +41,31 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
   plucky.toDestination();
+  pitch.toDestination();
+  plucky.connect(pitch);
 }
 
 function draw() {
   background(220);
-  image(img1,0,0,200,150);
+  image(img1,0,100,200,150);
   if(boolean){
-    image(img2,190,30,80,80,0,0,50,40);
+    image(img2,190,30,200,200,0,0,50,40);
   }else{
-    image(img2,200,30,80,100,140,150,50,50);
+    image(img2,200,30,200,200,140,150,50,50);
   }
+
+  text("press mouse to kill Chill Penguin", 200, 300);
 
 }
 
 function mousePressed() {
+  
   boolean = false;
   setTimeout(()=>{
     boolean = true;
-  }, 300)
+  }, 500)
+
+
+   play();
 
 }
